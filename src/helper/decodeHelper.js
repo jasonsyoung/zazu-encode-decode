96e419f10145035f25416c22af7a3a4a39d326fe
Blaine Schmeisser <blainesch@gmail.com>
2017-01-25 12:10:48 -0800

    Convert helpers to keywords.

--- src/helper/decodeHelper.js
+++
@@ -1,9 +0,0 @@
-module.exports = pluginContext => (query, env = {}) => new Promise((resolve, reject) => {
-  resolve([
-    {
-      icon: 'fa-file-code-o',
-      title: 'Decode',
-      subtitle: 'Decodes a string into multiple formats (Usage: decode mystring)',
-    },
-  ]);
-});
--- src/helper/encodeHelper.js
+++
@@ -1,9 +0,0 @@
-module.exports = pluginContext => (query, env = {}) => new Promise((resolve, reject) => {
-  resolve([
-    {
-      icon: 'fa-file-code-o',
-      title: 'Encode',
-      subtitle: 'Encodes a string into multiple formats (Usage: encode mystring)',
-    },
-  ]);
-});
